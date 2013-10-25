64f371ddd2a163ac7e654f0992b8d3a808627b92
sergio1990 <sergeg1990@gmail.com>
2013-10-25 18:47:03 +0300

    -Refactor LuaScripts module

--- lib/active_redis/helpers/lua_scripts.rb
+++ lib/active_redis/helpers/lua_scripts.rb
@@ -2,39 +2,13 @@ module ActiveRedis
   module Helpers
     module LuaScripts
 
-      def count_script
-        return <<-COUNT
-          #{LuaLoader.get_main}
-          return calculate_count(KEYS[1])
-        COUNT
-      end
-
-      def pluck_script
-        return <<-COUNT
-          #{LuaLoader.get_main}
-          return calculate_pluck(KEYS[1], ARGV[1])
-        COUNT
-      end
-
-      def min_script
-        return <<-COUNT
-          #{LuaLoader.get_main}
-          return calculate_min(KEYS[1], ARGV[1])
-        COUNT
-      end
-
-      def max_script
-        return <<-COUNT
-          #{LuaLoader.get_main}
-          return calculate_max(KEYS[1], ARGV[1])
-        COUNT
-      end
-
-      def sum_script
-        return <<-COUNT
-          #{LuaLoader.get_main}
-          return calculate_sum(KEYS[1], ARGV[1])
-        COUNT
+      %w{count pluck min max sum}.each do |method|
+        define_method "#{method}_script" do
+          <<-LUA
+            #{LuaLoader.get_main}
+            return calculate_#{method}(KEYS[1]#{method == "count" ? "" : ", ARGV[1]"})
+          LUA
+        end
       end
 
       class LuaLoader
