031f0f66125bafce409d38566648e4a0aefdd19a
sergio1990 <sergeg1990@gmail.com>
2013-10-31 19:18:39 +0200

    -Configured MiniTest and added test for ActiveRedis::Config class

---
+++ specs/config_spec.rb
@@ -0,0 +1,22 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'active_redis/config'
+
+describe ActiveRedis::Config do
+
+  subject { ActiveRedis::Config.new }
+
+  let(:options) { :connection_options }
+  let(:adapter) { :adapter }
+
+  it "is has connection_options attribute" do
+    subject.connection_options = options
+    subject.connection_options.must_be_same_as options
+  end
+
+  it "is has adapter attribute" do
+    subject.adapter = adapter
+    subject.adapter.must_be_same_as adapter
+  end
+
+end
