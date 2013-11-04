1e87ed3333c323a92619d5613c5665a02ad004ff
sergio1990 <sergeg1990@gmail.com>
2013-11-04 23:23:16 +0200

    -Added executing query after chaining

--- lib/active_redis/query_chainer.rb
+++ lib/active_redis/query_chainer.rb
@@ -1,14 +1,17 @@
 require 'active_redis/relation'
 
 module ActiveRedis
+  autoload :QueryExecutor, 'active_redis/query_executor'
+
   class QueryChainer
     include Relation
 
     def initialize(target)
       @where_options = {}
       @order_options = {id: :asc}
-      @limit_options = []
+      @limit_options = {per_page: 10, page: 0}
       @target = target
+      @first = false
     end
 
     def apply_where(options)
@@ -23,7 +26,37 @@ module ActiveRedis
 
     def apply_limit(options)
       @limit_options = options
+      @first = false
       self
+    end
+
+    def apply_first(options)
+      @limit_options = {per_page: 1, page: 0}
+      @first = true
+      self
+    end
+
+    def inspect
+      if object.is_a? Array
+        "[#{object.map{|e| e.inspect}.join(', ')}]"
+      else
+        object.inspect
+      end
+    end
+
+    private
+
+    def object
+      @object ||= objects_by_query
+    end
+
+    def execute_query
+      QueryExecutor.execute(@target, @where_options, @order_options, @limit_options)
+    end
+
+    def objects_by_query
+      res = execute_query.map { |attrs| @target.new(attrs) }
+      @first ? res.first : res
     end
 
   end
---
+++ lib/active_redis/query_executor.rb
@@ -0,0 +1,24 @@
+module ActiveRedis
+  class QueryExecutor
+
+    def self.execute(target, where, order, limit)
+      ActiveRedis.connection.run_query_analyzer target, [prepare_where(where), prepare_order(order), prepare_limit(limit)]
+    end
+
+    private
+
+    def self.prepare_where(where)
+      where.keys.map{|key| "#{key}=#{where[key]}" }.join(", ")
+    end
+
+    def self.prepare_order(order)
+      field = order.keys.first
+      "#{field}=#{order[field]}"
+    end
+
+    def self.prepare_limit(limit)
+      limit.any? ? "#{limit[:page]}=#{limit[:per_page]}" : ""
+    end
+
+  end
+end
