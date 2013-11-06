3a841e7a507b1c5dc389ad4559a74b8a78ab81bf
sergio1990 <sergeg1990@gmail.com>
2013-11-06 11:30:20 +0200

    -Added benchmarking run_query_analyzer

--- lib/active_redis/connection_ext/finders_layer.rb
+++ lib/active_redis/connection_ext/finders_layer.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 module ActiveRedis::ConnectionExt
   module FindersLayer
 
@@ -16,8 +18,12 @@ module ActiveRedis::ConnectionExt
     end
 
     def run_query_analyzer(model, params = ["", "", ""])
-      ActiveRedis.log.write "Calling ActiveRedis::ConnectionExt::FindersLayer::run_query_analyzer(#{model.name}, #{params})"
-      run_eval :query_analyzer, [model.key_name, Time.now.to_i], params
+      res = nil
+      time = Benchmark.realtime do
+        res = run_eval :query_analyzer, [model.key_name, Time.now.to_i], params
+      end
+      ActiveRedis.log.write "Redis Query (#{(time*1000).round(2)}) => run_query_analyzer(#{model.name}, #{params})"
+      res
     end
 
   end
